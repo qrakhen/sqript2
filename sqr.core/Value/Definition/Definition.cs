a7eae1440bd40df6f771c2f494bbf6c6d721d84b
David Neumaier <filosofem.dave@gmail.com>
2022-09-16 20:37:04 +0200

    Add new file

---
+++ sqr.core/Value/Definition/Definition.cs
@@ -0,0 +1,26 @@
+public class Definition
+{
+    public string name;
+
+    public members
+
+    public void call(
+    
+}
+
+public class Qlass : Definition
+ 
+
+public class Instance : Value
+{
+    public fields
+
+public class Value
+     ...definition
+    
+move is primitive and stuff to defintion
+
+
+
+
+
