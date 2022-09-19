12b9c1b3a468d31d28c7b61d254d95d2146c161b
Qrakhen <filosofem.dave@gmail.com>
2022-09-19 02:59:47 +0200

    aksk

--- sqr.core/Resolver/Objeqt.cs
+++ sqr.core/Resolver/Objeqt.cs
@@ -1,8 +1,5 @@
 ﻿using Qrakhen.Dependor;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
 
 namespace Qrakhen.Sqr.Core
 {
--- sqr.core/Resolver/Value.cs
+++ sqr.core/Resolver/Value.cs
@@ -60,7 +60,7 @@ namespace Qrakhen.Sqr.Core
                             qontext),
                         qontext);
                     log.spam("parameters: " + parameters);
-                    value = ((value as Value<Funqtion>).raw as InternalFunqtion).execute(new Funqtion.ProvidedParam[0], parent.obj); //@TODO make this less awful
+                    value = ((value.obj as Value<Funqtion>).raw as InternalFunqtion).execute(new Funqtion.ProvidedParam[0], (parent == null ? value : parent).obj); //@TODO make this less awful
                 }
 
                 if (input.done)
