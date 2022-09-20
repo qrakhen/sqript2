4b155ff0cab2f7abf2c3c9829addd04b5ea2191e
Qrakhen <filosofem.dave@gmail.com>
2022-09-20 04:45:40 +0200

    qlaslas

---
+++ sqr.core/Resolver/Qlass.cs
@@ -0,0 +1,20 @@
+﻿using Qrakhen.Dependor;
+using System.Linq;
+
+namespace Qrakhen.Sqr.Core
+{
+    [Injectable]
+    public class QlassResolver : Resolver<Stack<Token>, Objeqt>
+    {
+        private readonly ValueResolver valueResolver;
+        private readonly StructureResolver structureResolver;
+        private readonly OperationResolver operationResolver;
+
+        public Objeqt resolve(Stack<Token> input, Qontext qontext)
+        {
+            log.spam("in " + GetType().Name);
+            
+            return objeqt;
+        }
+    }
+}
---
+++ sqr.core/Value/Native/Qlass.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using Qrakhen.Dependor;
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Qrakhen.Sqr.Core
+{
+    public class Qlass : Value
+    {
+
+    }
+}
