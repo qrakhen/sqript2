89336e38b9c8df461a8ab5b0fe59ef081d3c909b
Qrakhen <filosofem.dave@gmail.com>
2022-09-23 13:30:01 +0200

    native gettter

--- sqr.core/Value/Definition/Native.cs
+++ sqr.core/Value/Definition/Native.cs
@@ -5,4 +5,5 @@ namespace Qrakhen.Sqr.Core
     public class NativeAttribute : Attribute { }
     public class NativeMethodAttribute : NativeAttribute { }
     public class NativeFieldAttribute : NativeAttribute { }
+    public class NativeGetterAttribute : NativeFieldAttribute { }
 }
