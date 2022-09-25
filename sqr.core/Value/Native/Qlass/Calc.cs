3cc56a08c48f74ddcec63530983445c6fc8aff9a
KaNaDaAT <kalian.danzer@gmail.com>
2022-09-25 03:05:15 +0200

    Update Calc.cs

--- sqr.core/Value/Native/Qlass/Calc.cs
+++ sqr.core/Value/Native/Qlass/Calc.cs
@@ -26,6 +26,21 @@ namespace Qrakhen.Sqr.Core
             return new Number(Math.Sqrt(value));
         }
 
+        [NativeMethod]
+        public static Number pow(Number value1, Number value2) {
+            return new Number(Math.Pow(value1, value2));
+        }
+
+        [NativeMethod]
+        public static Number log(Number value) {
+            return new Number(Math.Log(value));
+        }
+
+        [NativeMethod]
+        public static Number log2(Number value) {
+            return new Number(Math.Log2(value));
+        }
+
         static Calc()
         {
             Type.register(typeof(Calc), new Type.Args {
